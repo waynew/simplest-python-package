0e6cbc9750565ded27ca532e4d85acf8b519c282
Edward Zion Saji <37019147+Edward-Zion-Saji@users.noreply.github.com>
2018-10-19 20:33:53 +0530

    Updated
    Updated. Added the functionality to stop running. Hehe

--- myproj/__main__.py
+++ myproj/__main__.py
@@ -1,5 +1,17 @@
+
 def run():
     print("Yep, I'm running!")
+    user_in = input("type stop then you want me to stop")
+    if user_in == "stop"
+    stop()
+    
+def stop():
+    print("stopped")
+    exit()
+        
+    
+    
+  
 
 
 if __name__ == '__main__':
